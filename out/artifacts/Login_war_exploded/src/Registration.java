4e80e5aa160e81337eecd60e9d5529539ddabb19
Martin Ruud <m.ruud93@gmail.com>
2019-09-18 22:01:56 +0200

    Commit

--- out/artifacts/Login_war_exploded/src/Registration.java
+++ out/artifacts/Login_war_exploded/src/Registration.java
@@ -14,6 +14,7 @@ public class Registration extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+        //Getting parameters
         String name = request.getParameter("name");
         String pass = request.getParameter("pass");
         try {
