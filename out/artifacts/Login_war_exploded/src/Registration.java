5b9526f68bd9da261158e1e3ffeaa722999271f8
Martin Ruud <m.ruud93@gmail.com>
2019-09-18 13:29:31 +0200

    Servlet fixed

---
+++ out/artifacts/Login_war_exploded/src/Registration.java
@@ -0,0 +1,43 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+
+@WebServlet("/Registration")
+public class Registration extends HttpServlet {
+    String sql = "INSERT INTO login (uname, pass) VALUES (?,?,?)";
+    String url = "jdbc:mysql://localhost:3306/javaTest";
+    String username = "root";
+    String password = "siemensmobil2";
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String name = request.getParameter("name");
+        String pass = request.getParameter("pass");
+        try {
+            //Initialize the DB
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, username, password);
+
+            //Choose the input for the database
+            PreparedStatement st = con.prepareStatement(sql);
+            st.setString(1, name);
+            st.setString(2,pass);
+
+            st.executeUpdate();
+
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        response.sendRedirect("login.jsp");
+
+    }
+}
+
+
