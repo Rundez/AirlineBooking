e3a011fa8d551241d54a8aa4474acf92087f1c65
Martin Ruud <m.ruud93@gmail.com>
2019-09-25 20:59:42 +0200

    Working on generating flights

--- web/src/Controllers/FlightController.java
+++ web/src/Controllers/FlightController.java
@@ -1,4 +1,32 @@
 package Controllers;
 
+import Classes.Flight;
+
 public class FlightController {
+    private Flight flight = new Flight();
+
+    public Flight createReturnFlight(){
+        return flight;
+    }
+
+    public void setArrivalTime(String arrivalTime){
+        flight.setArrivalTime(arrivalTime);
+    }
+
+    public void setDepartureTime(String departureTime){
+        flight.setDepartureTime(departureTime);
+    }
+
+    public void setAirplaneID(int airplaneID){
+        flight.setAirplaneID(airplaneID);
+    }
+
+    public void setDestinationID(int destinationID){
+        flight.setDepartureID(destinationID);
+    }
+
+    public void setArrivalID(int arrivalID){
+        flight.setArrivalID(arrivalID);
+    }
+
 }
