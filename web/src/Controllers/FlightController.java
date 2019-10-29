ea3687b7417c451344ea6a662a8d569693d14b4b
Inoobme <kantai18@uia.no>
2019-10-29 20:26:44 +0100

    Fikser små ting for å få ting til å se smooth ut!

--- web/src/Controllers/FlightController.java
+++ web/src/Controllers/FlightController.java
@@ -1,9 +1,7 @@
 package Controllers;
 
 import Classes.Flight;
-
 import DB.FlightDAO;
-import org.jetbrains.annotations.NotNull;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
